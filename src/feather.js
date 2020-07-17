7584a23d8fdcf872970ba7d8043abe4235b3dcc5
nickgarfield <ntgarfield@gmail.com>
2020-07-16 23:05:36 -0500

    Remove usused feather.js file

--- src/feather.js
+++
@@ -1,9 +0,0 @@
-// import { Feather } from "feather-client-react";
-//
-// export const feather = Feather("");
-//
-// const unsubscribe = feather.onStateChange(user => {
-//   sessionStorage.setItem("idToken", user ? user.tokens.idToken : null);
-// });
-//
-// unsubscribe();
